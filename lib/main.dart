74f59823e8de2d5d76476b80ba188f952a790cf0
Brenno marques <brenno.lima@soluti.com.br>
2020-08-19 08:06:34 -0300

    Feito MAP das perguntas

--- lib/main.dart
+++ lib/main.dart
@@ -6,41 +6,58 @@ main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+
+  final _perguntas = const [
+    {
+      'texto': 'Qual e sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual seu animal favorido?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual seu instrutor favorido?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    }
+  ];
+
   void _eResponder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (ePerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get ePerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual e sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual seu animal favorido?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual seu instrutor favorido?',
-        'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-      }
-    ];
+    List<String> respostas = ePerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Perguntas")),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(perguntas[_perguntaSelecionada]['texto']),
-            Answers('Reposta 1', _eResponder),
-            Answers('Reposta 2', _eResponder),
-            Answers('Reposta 3', _eResponder),
-          ],
-        ),
+        body: ePerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Question(_perguntas[_perguntaSelecionada]['texto']),
+                  ...respostas.map((e) => Answers(e, _eResponder)).toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
